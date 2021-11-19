6184678b81d1b201215d5615a679490e98a1b94e
Andres <aneduardotejada@gmail.com>
2021-11-18 21:22:32 -0500

    paginas agregadas

---
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -0,0 +1,7 @@
+export const LazyPage2 = () => {
+    return (
+        <h1>
+           lazy page2
+        </h1>
+    )
+}
---
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -0,0 +1,7 @@
+export const LazyPage3 = () => {
+    return (
+        <h1>
+           lazy page3
+        </h1>
+    )
+}
